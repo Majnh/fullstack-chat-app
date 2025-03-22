fcb059e0b9022030dfa3c49a5857add879a6ea42
Manh <ngocmanhdc@gmail.com>
2025-03-22 18:17:09 +0700

    change limit uploadImg

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const _dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
